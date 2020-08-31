0de6808c7567ebefcbe7b4aad32920e7d8bd6527
Joshua Lovelace <rakmial@gmail.com>
2020-08-31 15:45:43 -0700

    test 3 green

--- lib/PrimeSieve.rb
+++ lib/PrimeSieve.rb
@@ -8,4 +8,11 @@ class PrimeSieve
     @right_bound = right_bound
     @sieve_range = 2..right_bound
   end
+
+  def get_primes
+    @sieve_range.each do |e|
+      @sieve_range = @sieve_range.reject { |f| ((f / e >= 2) && (f % e == 0))}
+    end
+    @sieve_range
+  end
 end
