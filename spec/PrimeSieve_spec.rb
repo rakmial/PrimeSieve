8da5f60e1d8fa4ff0bfb4721094b08f5b675de35
Joshua Lovelace <rakmial@gmail.com>
2020-08-31 15:17:55 -0700

    test 3 red

--- spec/PrimeSieve_spec.rb
+++ spec/PrimeSieve_spec.rb
@@ -8,4 +8,7 @@ describe('PrimeSieve') do
   it('inits instance var @sieve_range = 2..right_bound with reader attr') do
     expect(PrimeSieve.new(7).sieve_range).to(eq(2..7))
   end
+  it('provides method .get_primes which returns array with primes <= right_bound') do
+    expect(PrimeSieve.new(7).get_primes).to(eq([2, 3, 5, 7]))
+  end
 end
