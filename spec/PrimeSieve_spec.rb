d4c4023a53c753f8302fb20be0df908b98480781
Joshua Lovelace <rakmial@gmail.com>
2020-08-31 15:15:28 -0700

    test 2 red

--- spec/PrimeSieve_spec.rb
+++ spec/PrimeSieve_spec.rb
@@ -5,4 +5,7 @@ describe('PrimeSieve') do
   it('inits with int input right_bound with reader attr') do
     expect(PrimeSieve.new(7).right_bound).to(eq(7))
   end
+  it('inits instance var @sieve_range = 2..right_bound with reader attr') do
+    expect(PrimeSieve.new(7).sieve_range).to(eq(2..7))
+  end
 end
