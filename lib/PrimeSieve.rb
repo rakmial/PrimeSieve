73e03f2aa681c8e69abdfc85c88c2617dd587c60
Joshua Lovelace <rakmial@gmail.com>
2020-08-31 15:16:07 -0700

    test 2 green

--- lib/PrimeSieve.rb
+++ lib/PrimeSieve.rb
@@ -2,9 +2,10 @@ require ('pry')
 require ('rspec')
 
 class PrimeSieve
-  attr_reader(:right_bound)
+  attr_reader(:right_bound, :sieve_range)
 
   def initialize(right_bound)
     @right_bound = right_bound
+    @sieve_range = 2..right_bound
   end
 end
